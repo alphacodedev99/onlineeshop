a71d3221284b2d7e01404ecce5b61a61a936de58
alphacodedev99 <kiza.milenkovic@gmail.com>
2024-06-23 13:45:21 +0200

    Header & Navbar with responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mainBlue': '#003F62',
+        'mainYellow': '#EDA415',
+        'ligthGray': '#F4F4F4',
+        'textWhite': '#FFF',
+        'textDark': '#292D32',
+        'ligthBlue': '#B3D4E5'
+      }
+    },
   },
   plugins: [],
 }
